8312ab81a9b8889667a171c4e2dda7cf5fd6dad5
Davide <doctegl@gmail.com>
2021-06-04 20:31:09 +0200

    kernel (probably corrupted build.bat)

--- env/build.bat
+++ env/build.bat
@@ -4,4 +4,6 @@ IF NOT EXIST ..\build mkdir ..\build
 REM pushd ..\build
 nasm -fbin ..\code\boot.asm -o ..\build\boot.bin
 
+cl 
+
 REM popd
--- env/shell.bat
+++ env/shell.bat
@@ -1,5 +1,5 @@
 @echo off
-REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 set path=x:\clean_os\env;"c:\Program Files\NASM";"C:\Program Files\Bochs-2.6.11";%path%
 set DIRCMD=/o
 set HOME=x:\clean_os\env\vimconf
