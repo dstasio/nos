04de86ae0460a1f96b9db5382fd62f46ad665486
Davide Stasio <doctegl@gmail.com>
2023-11-26 11:57:41 +0100

    backup

--- env/build.bat
+++ env/build.bat
@@ -1,9 +1,10 @@
 @echo off
 
 IF NOT EXIST ..\build mkdir ..\build
-REM pushd ..\build
+pushd ..\build
 nasm -fbin ..\code\boot.asm -o ..\build\boot.bin
 
-cl 
+gcc -ffreestanding -c ../code/kernel.c -o kernel.o
+ld -o kernel.bin -Ttext 0x1000 kernel.o --oformat binary
 
-REM popd
+popd
--- env/shell.bat
+++ env/shell.bat
@@ -1,5 +1,5 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
-set path=x:\clean_os\env;"c:\Program Files\NASM";"C:\Program Files\Bochs-2.6.11";%path%
+set path=x:\nos\env;c:\dev\NASM;%path%
 set DIRCMD=/o
-set HOME=x:\clean_os\env\vimconf
+set HOME=x:\nos\env\vimconf
